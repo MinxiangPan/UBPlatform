39b270894354496cc93c7d2db2c48ab281a394cf
Bousine <asif21hasan@gmail.com>
2019-04-22 07:11:27 -0400

    Back & refresh button work-in-progress

--- project/src/index.js
+++ project/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
@@ -11,3 +12,19 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+function goBack() {
+        if (History.length > 1 || location === RefreshLocation) {
+          location.pop();
+          return true;
+        }
+
+        warning(false, 'goBack() was ignored because there is no router history');
+
+        return false;
+}
+
+function refresh(){
+  Router.dispatch(location.getCurrentPath(), null);
+}
