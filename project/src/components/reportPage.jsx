5f81f3e6f1202c737559dc52503d389fdeb19472
matt <pmx971114@gmail.com>
2019-05-07 22:38:58 -0400

    Fix reportPage where it's been overwrite.

--- project/src/components/reportPage.jsx
+++ project/src/components/reportPage.jsx
@@ -3,8 +3,23 @@ import axios from "axios";
 import "./reportStyle.css";
 
 class reportPage extends Component {
+  
+  state ={
+    seller: null,
+    email: null,
+    book: null,
+    reason: null
+  };
+
   reportSend = () => {
-    axios.post(this.props.api + "/report");
+    if(this.state.seller && this.state.email && this.state.book && this.state.reason){
+      axios.post(this.props.api + "/report", {seller :this.state.seller, email: this.state.email, book:this.state.book, reason:this.state.reason})
+      .then(res => {res.data.success ? alert("Report Sent!") : alert("Your need to fill up all the require fields.");})
+      .catch(err => alert("!error!"));
+    }
+    else {
+      alert("You need to fill up the all the require fields.");
+    }
   };
 
   render() {
@@ -14,43 +29,32 @@ class reportPage extends Component {
         <form
           class="form-style-9"
           style={{ textAlign: "center" }}
-          method="POST"
-          action="mailto:xliu72@buffalo.edu"
+          action=""
         >
           <label>
             Seller Username:
-            <input
-              type="text"
-              name="sellerName"
-              placeholder="seller username"
-            />
+            <input type="text" name="sellerName" onChange={(e)=> {this.setState({seller: e.target.value})}}/>
           </label>
           <br />
           <label>
             Seller Email:
-            <input type="text" name="sellerEmail" placeholder="seller email" />
+            <input type="text" name="sellerEmail" onChange={(e)=> {this.setState({email: e.target.value})}}/>
           </label>
           <br />
 
           <label>
             Book Name:
-            <input type="text" name="bookName" placeholder="book name" />
+            <input type="text" name="bookName" onChange={(e)=> {this.setState({book: e.target.value})}} />
           </label>
           <br />
           <label>
             Reason of report:
             <br />
-            <textarea
-              rows="4"
-              cols="50"
-              name="comment"
-              form="usrform"
-              placeholder="message"
-            />
+            <textarea rows="4" cols="50" name="comment" form="usrform" onChange={(e)=> {this.setState({reason: e.target.value})}}/>
           </label>
           <br />
 
-          <input type="submit" value="Submit" class="btn btn-primary" />
+          <input type="submit" value="Submit" onClick={()=>{this.reportSend()}}/>
         </form>
       </React.Fragment>
     );
